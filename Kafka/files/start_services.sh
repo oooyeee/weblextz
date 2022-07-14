df181b1a11a0bd5b5c9ac7c4885b69e3d9323b18
Yaroslav Minakov <iam.yaroslav@gmail.com>
2022-07-14 20:19:11 +0900

    testing heroku ports 3

--- Kafka/files/start_services.sh
+++ Kafka/files/start_services.sh
@@ -3,6 +3,14 @@ service zookeeper start
 sleep 2s
 # service kafka start
 
+LINE1="listeners=PLAINTEXT://:$PORT"
+LINE2="advertised.listeners=PLAINTEXT://oooyeee-test.herokuapp.com:$PORT"
+
+echo "got port $PORT" > /container/tmp/heerokuport
+
+echo "$LINE1" >> /container/kafkadir/config/server.properties
+echo "$LINE2" >> /container/kafkadir/config/server.properties
+
 isRunning="running"
 KAFKA_STATUS=/container/tmp/kafka_status
 
